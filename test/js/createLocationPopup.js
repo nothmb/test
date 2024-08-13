c4a30e1ec97eb1ad14f9cd827e74305d4ead5bfb
Alexey <105348635+nothmb@users.noreply.github.com>
2024-08-13 09:57:10 +0300

    Add files via upload

--- test/js/createLocationPopup.js
+++ test/js/createLocationPopup.js
@@ -7,3 +7,13 @@ document.getElementById("closePopupBtn").addEventListener("click", function() {
     document.getElementById("popupCreateLocation").style.display = "none";
     document.getElementById("overlay").style.display = "none";
 });
+
+document.getElementById("createLocationBtn").addEventListener("click", function() {
+    document.getElementById("popupCreateLocation").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+});
+
+document.getElementById("closePopupBtn").addEventListener("click", function() {
+    document.getElementById("popupCreateLocation").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+});
--- test/js/editLocationPopup.js
+++ test/js/editLocationPopup.js
@@ -1,9 +1,9 @@
 document.getElementById("editLocationBtn").addEventListener("click", function() {
     document.getElementById("editLocationPopup").style.display = "block";
     document.getElementById("overlay").style.display = "block";
-});
+});  
 
-document.getElementById("closePopupBtn").addEventListener("click", function() {
+document.getElementById("closeEditPopupBtn").addEventListener("click", function() {
     document.getElementById("editLocationPopup").style.display = "none";
     document.getElementById("overlay").style.display = "none";
 });
